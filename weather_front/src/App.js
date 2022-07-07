52002fc8a33dcbedcd374bb332e8c1769ecd5d2b
Vincent Moreschi <vmzcon@gmail.com>
2022-07-07 00:42:39 -0400

    clean up

--- weather_front/src/App.js
+++ weather_front/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let location = ''
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Simplr Weather Service</h1>
+        <input type="text" value ={location}/> <button>Submit</button>
+        
+
       </header>
     </div>
   );
