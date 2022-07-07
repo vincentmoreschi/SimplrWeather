b4db87388bb2593221c1e781ff67883872397ec2
Vincent Moreschi <vmzcon@gmail.com>
2022-07-07 16:41:15 -0400

    added initial components

--- weather_front/src/App.js
+++ weather_front/src/App.js
@@ -1,15 +1,20 @@
 //import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
   let location = ''
+  let temp = 81
   return (
     <div className="App">
       <header className="App-header">
         <h1>Simplr Weather Service</h1>
-        <input type="text" value ={location}/> <button>Submit</button>
-        
-
+        <body>
+        <p>Welome to the Simplr Weather Service, please enter your location to get the current weather, you will then be presented with the options to look at a 7 day forcast or enter a date for historical data</p>
+        <div className="container">
+        <input type="text" value ={location} placeholder= "Enter your location"/> <button>Submit</button>
+        </div>
+        </body>
       </header>
     </div>
   );
---
+++ weather_front/src/components/temperature.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+
+function Temperature({temp}) {
+    return (
+        <div>{temp}</div>
+      );  
+}
+
+
+export default Temperature;
